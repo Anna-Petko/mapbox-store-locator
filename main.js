94e85bbef026205943392da340ddb49def078e6d
Анна Петько <annapetko@icloud.com>
2018-11-13 23:26:45 +0100

    making map interactive

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// This will let you use the .remove() function later on
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5uYS1wIiwiYSI6ImNqb2V1Y29vYjIxMm0zcGxlZ2EyOGR6bzYifQ.dxEq0anALJ7E_Z7RoHH5XQ';
 
@@ -287,15 +295,30 @@ function buildLocationList(data) {
     var listings = document.getElementById('listings');
     var listing = listings.appendChild(document.createElement('div'));
     listing.className = 'item';
-    listing.id = 'listing' +i;
+    listing.id = 'listing' + i;
 
     // Create a new link with the class 'title' for each store
     // and fill it with the store address
     var link = listing.appendChild(document.createElement('a'));
-    link.href = '#';
-    link.className = 'title';
-    link.dataPosition = i;
-    link.innerHTML = prop.address;
+   link.href = '#';
+   link.className = 'title';
+   link.dataPosition = i;
+   link.innerHTML = prop.address;
+   // Add an event listener for the links in the sidebar listing
+link.addEventListener('click', function(e) {
+  // Update the currentFeature to the store associated with the clicked link
+  var clickedListing = data.features[this.dataPosition];
+  // 1. Fly to the point associated with the clicked link
+  flyToStore(clickedListing);
+  // 2. Close all other popups and display popup for clicked store
+  createPopUp(clickedListing);
+  // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+  var activeItem = document.getElementsByClassName('active');
+  if (activeItem[0]) {
+    activeItem[0].classList.remove('active');
+  }
+  this.parentNode.classList.add('active');
+});
 
     // Create a new div with the class 'details' for each store
     // and fill it with the city and phone number
@@ -304,9 +327,73 @@ function buildLocationList(data) {
     if(prop.phone) {
       details.innerHTML += ' &middot; ' + prop.phoneFormatted;
     }
-
-
-
-
   }
 }
+
+//Function to fly to the correct store
+function flyToStore(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 15
+  });
+}
+
+//Function to display popup features
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName ('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if(popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({closeOnClick: false})
+  .setLngLat(currentFeature.geometry.coordinates)
+  .setHTML('<h3>Sweetgreen</h3>' + '<h4>' + currentFeature.properties.address +'</h4>')
+  .addTo(map);
+}
+
+
+// Add an event listener for the links in the sidebar listing
+
+/*link.addEventListener('click', function(e) {
+  // Update the currentFeature to the store associated with the clicked link
+  var clickedListing = data.features[this.dataPosition];
+  // 1. Fly to the point associated with the clicked link
+  flyToStore(clickedListing);
+  // 2. Close all other popups and display popup for clicked store
+  createPopUp(clickedListing);
+  // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+  var activeItem = document.getElementByClassName('active');
+  if(activeItem[0]) {
+    activeItem[0].classList.remove('active');
+  }
+  this.parentNode.classList.add('active');
+});*/
+
+//// Add an event listener for when a user clicks on the map
+
+map.on('click', function(e) {
+  // Query all the rendered points in the view
+  var features = map.queryRenderedFeatures(e.point,{layers: ['locations']});
+  if(features.length) {
+    var clickedPoint = features[0];
+    // 1. Fly to the point
+    flyToStore(clickedPoint);
+    // 2. Close all other popups and display popup for clicked store
+    createPopUp(clickedPoint);
+    // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+    var activeItem = document.getElementsByClassName('active');
+    if(activeItem[0]) {
+      activeItem[0].classList.remove('active');
+    }
+    // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
+    var selectedFeature = clickedPoint.properties.address;
+
+    for (var i = 0; i < stores.features.length; i ++) {
+      if(stores.features[i].properties.address === selectedFeature) {
+        selectedFeatureIndex = i;
+      }
+    }
+    // Select the correct list item using the found index and add the active class
+    var listing = document.getElementById('listing' + selectedFeatureIndex);
+    listing.classList.add('active');
+  }
+});
