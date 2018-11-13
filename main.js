6efd0d68db164002ef0384129fc1449efab2b6a4
Анна Петько <annapetko@icloud.com>
2018-11-13 22:14:31 +0100

    adding store listing

--- main.js
+++ main.js
@@ -270,4 +270,43 @@ map.on('load', function(e) {
       'icon-allow-overlap': true,
     }
   });
+  buildLocationList(stores);
 });
+
+function buildLocationList(data) {
+  // Iterate through the list of stores
+  for(i=0; i < data.features.length; i++) {
+    var currentFeature = data.features[i];
+
+    // Shorten data.feature.properties to just `prop` so we're not
+    // writing this long form over and over again.
+    var prop = currentFeature.properties;
+
+    // Select the listing container in the HTML and append a div
+    // with the class 'item' for each store
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.className = 'item';
+    listing.id = 'listing' +i;
+
+    // Create a new link with the class 'title' for each store
+    // and fill it with the store address
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.dataPosition = i;
+    link.innerHTML = prop.address;
+
+    // Create a new div with the class 'details' for each store
+    // and fill it with the city and phone number
+    var details =listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.city;
+    if(prop.phone) {
+      details.innerHTML += ' &middot; ' + prop.phoneFormatted;
+    }
+
+
+
+
+  }
+}
